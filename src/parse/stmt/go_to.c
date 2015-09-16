e5783787812b1fbf4b842d91bb4731f8f641e33f
Ben Brewer <ben.brewer@codethink.co.uk>
2015-09-16 10:05:54 +0100

    Implement computed GO TO statement

--- src/parse/stmt/go_to.c
+++ src/parse/stmt/go_to.c
@@ -11,11 +11,7 @@ static unsigned parse_stmt_go_to_unconditional(
 	unsigned len = parse_label(
 		src, &ptr[i], &stmt->go_to.label);
 	if (len == 0)
-	{
-		sparse_error(src, &ptr[i],
-			"Expected label in GO TO statement");
 		return 0;
-	}
 
 	stmt->type = PARSE_STMT_GO_TO;
 	return (i + len);
@@ -77,6 +73,7 @@ static unsigned parse_stmt_go_to_assigned(
 			sizeof(parse_label_t) * (stmt->go_to_comp.label_count + 1));
 		if (!nlabel)
 		{
+			free(stmt->go_to_comp.label);
 			parse_stmt_cleanup(*stmt);
 			return 0;
 		}
@@ -96,6 +93,71 @@ static unsigned parse_stmt_go_to_assigned(
 	return i;
 }
 
+static unsigned parse_stmt_go_to_computed(
+	const sparse_t* src, const char* ptr,
+	parse_stmt_t* stmt)
+{
+	unsigned i = parse_keyword(
+		src, ptr, PARSE_KEYWORD_GO_TO);
+	if (i == 0) return 0;
+
+	if (ptr[i++] != '(')
+		return 0;
+
+	parse_label_t label;
+	unsigned len = parse_label(
+		src, &ptr[i], &label);
+	if (len == 0)
+		return 0;
+	i += len;
+
+	stmt->type = PARSE_STMT_GO_TO_COMPUTED;
+
+	stmt->go_to_comp.label_count = 1;
+	stmt->go_to_comp.label = (parse_label_t*)malloc(
+		sizeof(parse_label_t) * stmt->go_to_comp.label_count);
+	if (!stmt->go_to_comp.label)
+		return 0;
+	stmt->go_to_comp.label[0] = label;
+
+	while (ptr[i] == ',')
+	{
+		unsigned j = (i + 1);
+		len = parse_label(
+			src, &ptr[j], &label);
+		if (len == 0) break;
+
+		parse_label_t* nlabel = (parse_label_t*)realloc(stmt->go_to_comp.label,
+			sizeof(parse_label_t) * (stmt->go_to_comp.label_count + 1));
+		if (!nlabel)
+		{
+			free(stmt->go_to_comp.label);
+			return 0;
+		}
+		stmt->go_to_comp.label = nlabel;
+		stmt->go_to_comp.label[stmt->go_to_comp.label_count++] = label;
+		i = (j + len);
+	}
+
+	if ((ptr[i++] != ')')
+		|| (ptr[i++] != ','))
+	{
+		free(stmt->go_to_comp.label);
+		return 0;
+	}
+
+	len = parse_expr(
+		src, &ptr[i], &stmt->go_to_comp.cond);
+	if (len == 0)
+	{
+		free(stmt->go_to_comp.label);
+		return 0;
+	}
+	i += len;
+
+	return i;
+}
+
 unsigned parse_stmt_go_to(
 	const sparse_t* src, const char* ptr,
 	parse_stmt_t* stmt)
@@ -104,7 +166,9 @@ unsigned parse_stmt_go_to(
 		src, ptr, stmt);
 	if (len > 0) return len;
 
-	/* TODO - Implement GO TO (COMPUTED). */
+	len = parse_stmt_go_to_computed(
+		src, ptr, stmt);
+	if (len > 0) return len;
 
 	return parse_stmt_go_to_unconditional(
 		src, ptr, stmt);
