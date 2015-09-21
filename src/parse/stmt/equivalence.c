0f03299945c490c25bc2444c88199c722c83069c
Ben Brewer <ben.brewer@codethink.co.uk>
2015-09-21 12:09:51 +0100

    Parse EQUIVALENCE somewhat properly

--- src/parse/stmt/equivalence.c
+++ src/parse/stmt/equivalence.c
@@ -9,6 +9,9 @@ unsigned parse_stmt_equivalence(
 		src, ptr, PARSE_KEYWORD_EQUIVALENCE);
 	if (i == 0) return 0;
 
+	if (ptr[i++] !='(')
+		return 0;
+
 	stmt->type = PARSE_STMT_EQUIVALENCE;
 	stmt->equivalence.count = 0;
 	stmt->equivalence.name = NULL;
@@ -20,6 +23,17 @@ unsigned parse_stmt_equivalence(
 		(void*)parse_lhs_delete);
 	if (len == 0) return 0;
 	i += len;
+
+	if (ptr[i++] != ')')
+	{
+		parse_list_delete(src, ptr,
+			stmt->equivalence.count,
+			(void**)stmt->equivalence.name,
+			(void*)parse_lhs_delete);
+		return 0;
+	}
+
+	/* TODO - Parse lists of equivalence groups. */
 
 	return i;
 }
