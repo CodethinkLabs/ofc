3bf19bd029dd0a9e0abc340efa5faf0923232278
Ben Brewer <ben.brewer@codethink.co.uk>
2015-10-28 11:21:35 +0000

    Add some validation to go to

--- src/sema/stmt/go_to.c
+++ src/sema/stmt/go_to.c
@@ -5,16 +5,26 @@ ofc_sema_stmt_t* ofc_sema_stmt_go_to(
 	ofc_sema_scope_t* scope,
 	const ofc_parse_stmt_t* stmt)
 {
-	if (!scope || !stmt || (stmt->type != OFC_PARSE_STMT_GO_TO))
+	if (!scope || !stmt
+		|| (stmt->type != OFC_PARSE_STMT_GO_TO))
 		return NULL;
 
 	ofc_sema_stmt_t s;
 	s.type = OFC_SEMA_STMT_GO_TO;
-	s.go_to.label = ofc_sema_expr_label(scope, &stmt->go_to.label);
+	s.go_to.label = ofc_sema_expr_label(
+		scope, &stmt->go_to.label);
+	if (!s.go_to.label) return NULL;
+
+	if (!ofc_sema_expr_validate_uint(s.go_to.label))
+	{
+		ofc_sema_scope_error(scope, s.go_to.label->src,
+			"GO TO target must be a positive integer");
+		ofc_sema_expr_delete(s.go_to.label);
+		return NULL;
+	}
 
 	ofc_sema_stmt_t* as
 		= ofc_sema_stmt_alloc(s);
-
 	if (!as)
 	{
 		ofc_sema_expr_delete(s.go_to.label);
