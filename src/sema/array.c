31ee645f37a90ff557c2b76e1fdc1af1dc8afb94
Ben Brewer <ben.brewer@codethink.co.uk>
2016-01-28 10:39:50 +0000

    Remove invalid cleanup when creating cast expression

--- src/sema/array.c
+++ src/sema/array.c
@@ -80,7 +80,6 @@ ofc_sema_array_t* ofc_sema_array(
 				ofc_sema_expr_t* cast
 					= ofc_sema_expr_cast(seg->first,
 						ofc_sema_type_integer_default());
-				ofc_sema_expr_delete(seg->first);
 				if (!cast)
 				{
 					ofc_sema_array_delete(array);
