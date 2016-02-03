b2c387fa739006cb2ec08ff4183ef70071ab8e22
Violeta Menendez Gonzalez <violeta.menendez@codethink.co.uk>
2016-02-03 16:42:00 +0000

    Make PARAMETER declarations take the type of their RHS

--- src/sema/parameter.c
+++ src/sema/parameter.c
@@ -23,17 +23,62 @@ static ofc_sema_decl_t* ofc_sema_parameter__assign(
 	if (!scope || !assign)
 		return NULL;
 
-	ofc_sema_lhs_t* lhs = ofc_sema_lhs_local(
-		scope, assign->name);
-	if (!lhs) return NULL;
-
-	ofc_sema_decl_t* decl = lhs->decl;
-	ofc_sema_lhs_delete(lhs);
-	if (!decl) return NULL;
-	decl->is_parameter = true;
+	ofc_sparse_ref_t base_name;
+	if (!ofc_parse_lhs_base_name(
+		*(assign->name), &base_name))
+		return NULL;
 
 	ofc_sema_expr_t* init_expr = ofc_sema_expr(
 		scope, assign->init);
+	if (!init_expr) return NULL;
+
+	const ofc_sema_type_t* type
+		= ofc_sema_expr_type(init_expr);
+	if (!type)
+	{
+		ofc_sema_expr_delete(init_expr);
+		return NULL;
+	}
+
+	ofc_sema_spec_t* spec
+		= ofc_sema_scope_spec_modify(
+			scope, base_name);
+	if (!spec)
+	{
+		ofc_sema_expr_delete(init_expr);
+		return NULL;
+	}
+
+	if (spec->type_implicit)
+	{
+		spec->type = type->type;
+		spec->type_implicit = false;
+	}
+	if (spec->kind == 0)
+		spec->kind = type->kind;
+	if ((spec->len == 0) && !spec->len_var)
+	{
+		spec->len      = type->len;
+		spec->len_var  = type->len_var;
+	}
+
+
+	ofc_sema_lhs_t* lhs = ofc_sema_lhs_local(
+		scope, assign->name);
+	if (!lhs)
+	{
+		ofc_sema_expr_delete(init_expr);
+		return NULL;
+	}
+
+	ofc_sema_decl_t* decl = lhs->decl;
+	ofc_sema_lhs_delete(lhs);
+	if (!decl)
+	{
+		ofc_sema_expr_delete(init_expr);
+		return NULL;
+	}
+	decl->is_parameter = true;
 
 	bool success = ofc_sema_decl_init(
 		decl, init_expr);
