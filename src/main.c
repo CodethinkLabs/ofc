0b48057197bd4a3f43e6277b37c86eda017985ce
Craig Griffiths <craig.griffiths@codethink.co.uk>
2016-03-31 15:51:23 +0100

    Add file_list to main.c

--- src/main.c
+++ src/main.c
@@ -32,128 +32,135 @@ int main(int argc, const char* argv[])
 
 	ofc_print_opts_t print_opts = OFC_PRINT_OPTS_DEFAULT;
 
-	ofc_file_t* file = NULL;
+	ofc_file_list_t* file_list = ofc_file_list_create();
 
 	if (!ofc_cliarg_parse(argc, argv,
-		&file, &print_opts, &global_opts))
+		&file_list, &print_opts, &global_opts))
 		return EXIT_FAILURE;
 
-	ofc_sparse_t* condense = ofc_prep(file);
-	if (!condense)
+	unsigned i;
+	for (i = 0; i < file_list->count; i++)
 	{
-		if (ofc_file_no_errors())
-			ofc_file_error(file, NULL, "Failed to preprocess source file");
-		return EXIT_FAILURE;
-	}
-	ofc_file_delete(file);
+		ofc_file_t* file = file_list[i];
 
-	ofc_parse_stmt_list_t* program
-		= ofc_parse_file(condense);
-	if (!program)
-	{
-		if (ofc_file_no_errors())
-			ofc_file_error(file, NULL, "Failed to parse program");
-		ofc_sparse_delete(condense);
-		return EXIT_FAILURE;
-	}
-
-	if (global_opts.parse_print)
-	{
-		ofc_colstr_t* cs = ofc_colstr_create(print_opts, 72, 0);
-		if (!ofc_parse_stmt_list_print(cs, 0, program))
-		{
-			ofc_file_error(file, NULL, "Failed to print parse tree");
-			ofc_parse_stmt_list_delete(program);
-			ofc_sparse_delete(condense);
-			return EXIT_FAILURE;
-		}
-		ofc_colstr_fdprint(cs, STDOUT_FILENO);
-		ofc_colstr_delete(cs);
-	}
-
-	ofc_sema_scope_t* sema = NULL;
-	if (!global_opts.parse_only)
-	{
-		sema = ofc_sema_scope_global(program);
-		if (!sema)
+		ofc_sparse_t* condense = ofc_prep(file);
+		if (!condense)
 		{
 			if (ofc_file_no_errors())
-				ofc_file_error(file, NULL, "Program failed semantic analysis");
-			ofc_parse_stmt_list_delete(program);
+				ofc_file_error(file, NULL, "Failed to preprocess source file");
+			return EXIT_FAILURE;
+		}
+		ofc_file_delete(file);
+
+		ofc_parse_stmt_list_t* program
+			= ofc_parse_file(condense);
+		if (!program)
+		{
+			if (ofc_file_no_errors())
+				ofc_file_error(file, NULL, "Failed to parse program");
 			ofc_sparse_delete(condense);
 			return EXIT_FAILURE;
 		}
-	}
 
-	/* TODO - Add a command line argument for this pass. */
-	if (!ofc_sema_pass_struct_type(sema))
-	{
-		ofc_file_error(file, NULL, "Failed 'STRUCTURE to TYPE' semantic pass");
-		ofc_sema_scope_delete(sema);
-		ofc_parse_stmt_list_delete(program);
-		ofc_sparse_delete(condense);
-		return EXIT_FAILURE;
-	}
-
-	/* TODO - Add a command line argument for this pass. */
-	if (!ofc_sema_pass_char_transfer(sema))
-	{
-		ofc_file_error(file, NULL, "Failed 'string cast TRANSFER' semantic pass");
-		ofc_sema_scope_delete(sema);
-		ofc_parse_stmt_list_delete(program);
-		ofc_sparse_delete(condense);
-		return EXIT_FAILURE;
-	}
-
-	/* TODO - Add a command line argument for this pass. */
-	if (!ofc_sema_pass_unref_label(sema))
-	{
-		ofc_file_error(file, NULL, "Failed 'Remove unused labels' semantic pass");
-		ofc_sema_scope_delete(sema);
-		ofc_parse_stmt_list_delete(program);
-		ofc_sparse_delete(condense);
-		return EXIT_FAILURE;
-	}
-
-
-	/* TODO - Add a command line argument for this pass. */
-	if (!ofc_sema_pass_unlabelled_format(sema))
-	{
-		ofc_file_error(file, NULL, "Failed 'Remove unlabelled format statements' semantic pass");
-		ofc_sema_scope_delete(sema);
-		ofc_parse_stmt_list_delete(program);
-		ofc_sparse_delete(condense);
-		return EXIT_FAILURE;
-	}
-
-	/* TODO - Add a command line argument for this pass. */
-	if (!ofc_sema_pass_unlabelled_continue(sema))
-	{
-		ofc_file_error(file, NULL, "Failed 'Remove unlabelled continue statements' semantic pass");
-		ofc_sema_scope_delete(sema);
-		ofc_parse_stmt_list_delete(program);
-		ofc_sparse_delete(condense);
-		return EXIT_FAILURE;
-	}
-
-	if (global_opts.sema_print)
-	{
-		ofc_colstr_t* cs = ofc_colstr_create(print_opts, 72, 0);
-		if (!ofc_sema_scope_print(cs, 0, sema))
+		if (global_opts.parse_print)
 		{
-			ofc_file_error(file, NULL, "Failed to print semantic tree");
+			ofc_colstr_t* cs = ofc_colstr_create(print_opts, 72, 0);
+			if (!ofc_parse_stmt_list_print(cs, 0, program))
+			{
+				ofc_file_error(file, NULL, "Failed to print parse tree");
+				ofc_parse_stmt_list_delete(program);
+				ofc_sparse_delete(condense);
+				return EXIT_FAILURE;
+			}
+			ofc_colstr_fdprint(cs, STDOUT_FILENO);
 			ofc_colstr_delete(cs);
+		}
+
+		ofc_sema_scope_t* sema = NULL;
+		if (!global_opts.parse_only)
+		{
+			sema = ofc_sema_scope_global(program);
+			if (!sema)
+			{
+				if (ofc_file_no_errors())
+					ofc_file_error(file, NULL, "Program failed semantic analysis");
+				ofc_parse_stmt_list_delete(program);
+				ofc_sparse_delete(condense);
+				return EXIT_FAILURE;
+			}
+		}
+
+		/* TODO - Add a command line argument for this pass. */
+		if (!ofc_sema_pass_struct_type(sema))
+		{
+			ofc_file_error(file, NULL, "Failed 'STRUCTURE to TYPE' semantic pass");
 			ofc_sema_scope_delete(sema);
 			ofc_parse_stmt_list_delete(program);
 			ofc_sparse_delete(condense);
 			return EXIT_FAILURE;
 		}
-		ofc_colstr_fdprint(cs, STDOUT_FILENO);
-		ofc_colstr_delete(cs);
+
+		/* TODO - Add a command line argument for this pass. */
+		if (!ofc_sema_pass_char_transfer(sema))
+		{
+			ofc_file_error(file, NULL, "Failed 'string cast TRANSFER' semantic pass");
+			ofc_sema_scope_delete(sema);
+			ofc_parse_stmt_list_delete(program);
+			ofc_sparse_delete(condense);
+			return EXIT_FAILURE;
+		}
+
+		/* TODO - Add a command line argument for this pass. */
+		if (!ofc_sema_pass_unref_label(sema))
+		{
+			ofc_file_error(file, NULL, "Failed 'Remove unused labels' semantic pass");
+			ofc_sema_scope_delete(sema);
+			ofc_parse_stmt_list_delete(program);
+			ofc_sparse_delete(condense);
+			return EXIT_FAILURE;
+		}
+
+
+		/* TODO - Add a command line argument for this pass. */
+		if (!ofc_sema_pass_unlabelled_format(sema))
+		{
+			ofc_file_error(file, NULL, "Failed 'Remove unlabelled format statements' semantic pass");
+			ofc_sema_scope_delete(sema);
+			ofc_parse_stmt_list_delete(program);
+			ofc_sparse_delete(condense);
+			return EXIT_FAILURE;
+		}
+
+		/* TODO - Add a command line argument for this pass. */
+		if (!ofc_sema_pass_unlabelled_continue(sema))
+		{
+			ofc_file_error(file, NULL, "Failed 'Remove unlabelled continue statements' semantic pass");
+			ofc_sema_scope_delete(sema);
+			ofc_parse_stmt_list_delete(program);
+			ofc_sparse_delete(condense);
+			return EXIT_FAILURE;
+		}
+
+		if (global_opts.sema_print)
+		{
+			ofc_colstr_t* cs = ofc_colstr_create(print_opts, 72, 0);
+			if (!ofc_sema_scope_print(cs, 0, sema))
+			{
+				ofc_file_error(file, NULL, "Failed to print semantic tree");
+				ofc_colstr_delete(cs);
+				ofc_sema_scope_delete(sema);
+				ofc_parse_stmt_list_delete(program);
+				ofc_sparse_delete(condense);
+				return EXIT_FAILURE;
+			}
+			ofc_colstr_fdprint(cs, STDOUT_FILENO);
+			ofc_colstr_delete(cs);
+		}
+
+		ofc_sema_scope_delete(sema);
+		ofc_parse_stmt_list_delete(program);
+		ofc_sparse_delete(condense);
 	}
 
-	ofc_sema_scope_delete(sema);
-	ofc_parse_stmt_list_delete(program);
-	ofc_sparse_delete(condense);
 	return EXIT_SUCCESS;
 }
