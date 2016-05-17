ecf95e834f93e7b9df1e0888d41d53836a064506
Craig Griffiths <craig.griffiths@codethink.co.uk>
2016-05-17 17:14:07 +0100

    Negate .NOT. in INTEGER to LOGICAL cast pass

--- src/sema/pass/integer_logical.c
+++ src/sema/pass/integer_logical.c
@@ -15,30 +15,57 @@
 
 #include "ofc/sema.h"
 
+static bool ofc_sema_pass_integer_logical__is_logical_cast(
+	ofc_sema_expr_t* expr)
+{
+	return (expr->type == OFC_SEMA_EXPR_CAST)
+		&& (expr->cast.type->type == OFC_SEMA_TYPE_LOGICAL
+		&& ofc_sema_expr_type_is_integer(expr->cast.expr));
+}
+
+static bool ofc_sema_pass_integer_logical__expand(
+	ofc_sema_expr_t* expr)
+{
+	ofc_sema_expr_t* a = expr->cast.expr;
+	ofc_sema_expr_t* b = ofc_sema_expr_integer(0, OFC_SEMA_KIND_DEFAULT);
+	/* TODO - Work out side a kind and use the same for side b*/
+
+	if (!a || !b) return false;
+
+	expr->type = OFC_SEMA_EXPR_NE;
+	expr->a = a;
+	expr->b = b;
+
+	return true;
+}
 
 static bool ofc_sema_pass_integer_logical__expr(
 	ofc_sema_expr_t* expr, void* param)
 {
 	(void)param;
 
-	if (!expr)
-		return false;
+	if (!expr) return false;
 
-	if ((expr->type == OFC_SEMA_EXPR_CAST)
-		&& ofc_sema_expr_type_is_integer(expr->cast.expr)
-		&& (expr->cast.type->type == OFC_SEMA_TYPE_LOGICAL))
-		/* Check explicitly for LOGICAL as we don't want to transform
-		   casts to BYTE which can evaluate to LOGICAL.*/
+	if (expr->type == OFC_SEMA_EXPR_NOT)
 	{
-		ofc_sema_expr_t* a = expr->cast.expr;
-		ofc_sema_expr_t* b = ofc_sema_expr_integer(0, OFC_SEMA_KIND_DEFAULT);
+		ofc_sema_expr_t* sub_expr = expr->a;
 
-		if (!a || !b)
+		if (ofc_sema_pass_integer_logical__is_logical_cast(sub_expr))
+		{
+			if (!ofc_sema_pass_integer_logical__expand(sub_expr))
+				return false;
+
+			expr->type = OFC_SEMA_EXPR_EQ;
+			expr->a = ofc_sema_expr_copy(sub_expr->a);
+			expr->b = ofc_sema_expr_copy(sub_expr->b);
+
+			ofc_sema_expr_delete(sub_expr);
+		}
+	}
+	else if (ofc_sema_pass_integer_logical__is_logical_cast(expr))
+	{
+		if (!ofc_sema_pass_integer_logical__expand(expr))
 			return false;
-
-		expr->type = OFC_SEMA_EXPR_NE;
-		expr->a = a;
-		expr->b = b;
 	}
 
 	return true;
@@ -49,8 +76,7 @@ static bool ofc_sema_pass_integer_logical__scope(
 {
 	(void)param;
 
-	if (!scope)
-		return false;
+	if (!scope) return false;
 
 	return ofc_sema_scope_foreach_expr(
 		scope, NULL, ofc_sema_pass_integer_logical__expr);
@@ -59,8 +85,7 @@ static bool ofc_sema_pass_integer_logical__scope(
 bool ofc_sema_pass_integer_logical(
 	ofc_sema_scope_t* scope)
 {
-	if (!scope)
-		return false;
+	if (!scope) return false;
 
 	return ofc_sema_scope_foreach_scope(
 		scope, NULL, ofc_sema_pass_integer_logical__scope);
